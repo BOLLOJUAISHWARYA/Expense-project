5264cec441f6847a86431bbed20a3cf609456320
Aishwarya B <bolloju.aishwarya@gmail.com>
2023-11-03 08:03:32 +0530

    shell scripting

--- practice.sh
+++ practice.sh
@@ -9,10 +9,10 @@ echo $(ls)
 echo 0 - $0
 echo 1 - $1
 echo 2 - $2
-echo "* - $*"
-echo * -$*
-echo '* - $*'
+echo 3 - "* - $*"
+echo 4 - * -$*
+echo 5 - '* - $*'
 
-echo '# - $#'
-echo "# - $#"
-echo # - $#
+echo 6 - '# - $#'
+echo 7 - "# - $#"
+echo 8 - # - $#
